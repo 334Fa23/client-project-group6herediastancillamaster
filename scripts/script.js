39f8dbadb35ceb8f30fd360c38de4d99153ed110
brihere <64750242+brihere@users.noreply.github.com>
2023-10-08 23:42:33 -0500

    Created pages, favicon, and began navigation

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// Hamburger Menu Function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("TTE-logo");
+    if(menu.style.display === "block" && logo.style.display === "none"){
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
+
